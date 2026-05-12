84ed4c42e9e31d4a3e7bcd5a581e12491135531a
Jimmy Wennlund <jimmy@wennlund.nu>
2026-05-12 22:28:13 +0200

    Add log framework

--- src/main.c
+++ src/main.c
@@ -5,44 +5,41 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
 
 #include "cc1101.h"
 
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
 static uint8_t buffer[64];
 
-int main(void)
-{
-	printk("TADO CC1101 Receiver\n");
+int main(void) {
+  LOG_INF("TADO CC1101 Receiver");
 
-	int ret = cc1101_init();
-	if (ret < 0) {
-		printk("CC1101 init failed: %d\n", ret);
-		return ret;
-	}
-	printk("Connection OK\n");
+  int ret = cc1101_init();
+  if (ret < 0) {
+    LOG_ERR("CC1101 init failed: %d", ret);
+    return ret;
+  }
+  LOG_INF("Connection OK");
 
-	/* Enter RX mode */
-	cc1101_set_rx();
-	printk("Listening for packets...\n");
+  /* Enter RX mode */
+  cc1101_set_rx();
+  LOG_INF("Listening for packets...");
 
-	while (1) {
-		if (cc1101_check_rx_fifo(100)) {
-			if (cc1101_check_crc()) {
-				uint8_t len = cc1101_receive_data(buffer);
-				if (len > 0) {
-					printk("\r\nLENGTH: %d RSSI: %d LQI: %d\r\n",
-					       len, cc1101_get_rssi(),
-					       cc1101_get_lqi());
+  while (1) {
+    if (cc1101_check_rx_fifo(100)) {
+      if (cc1101_check_crc()) {
+        uint8_t len = cc1101_receive_data(buffer);
+        if (len > 0) {
+          LOG_INF("LENGTH: %d RSSI: %d LQI: %d", len, cc1101_get_rssi(),
+                  cc1101_get_lqi());
 
-					for (int i = 0; i < len; i++) {
-						printk("%02X ", buffer[i]);
-					}
-					printk("\n");
-				}
-			}
-		}
-		k_yield();
-	}
-	return 0;
+          LOG_HEXDUMP_INF(buffer, len, "RX data");
+        }
+      }
+    }
+    k_yield();
+  }
+  return 0;
 }
